1eb29e57b1800373b76fb500d87922368f56d083
ma018ma <marzia018@libero.it>
2023-03-01 15:06:14 +0100

    ultimo

---
+++ elementicomuni.cpp
@@ -0,0 +1,36 @@
+/*Scrivi una funzione che prenda in input due array di interi
+e le loro lunghezze e restituisca il numero di elementi
+comuni ai due array. Ad esempio, se gli array di input
+sono {1, 2, 3, 4} e {2, 4, 6, 8}, la funzione dovrebbe
+restituire 2 perché ci sono due elementi comuni: 2 e 4.*/
+/*Scrivi una funzione che prenda in input un array di interi e
+la sua lunghezza e restituisca il numero di sequenze di
+interi crescenti all'interno dell'array. Ad esempio, se
+l'array di input è {1, 2, 3, 5, 4, 7}, la funzione dovrebbe
+restituire 2 perché ci sono due sequenze crescenti: {1, 2,
+3} e {4, 7}.*/
+#include <iostream>
+#include <string>
+#include <array>
+
+using namespace std;
+
+int main()
+{
+    int arrey1[] = {1, 2};
+    int lun = sizeof(arrey1) / sizeof(arrey1[0]);
+    int arrey2[] = {1, 3};
+    int len = sizeof(arrey2) / sizeof(arrey2[0]);
+    int valore = 0;
+    for (int i = 0; i < lun; i++) // per scorrere arrey con i
+    {
+        for (int g = 0; g < len; g++)
+        {
+            if (arrey1[i] == arrey2[g])
+            {
+                valore++;
+                cout << "i valori comuni sono:" << valore << endl;
+            }
+        }
+    }
+}
--- restituisciArreyParo.cpp
+++ restituisciArreyParo.cpp
@@ -9,29 +9,36 @@ int main()
 {
     int quanti;
     int j;
-    int listaInteri[5] = {2, 3, 4, 5, 6};                               // creo arrey
-    int lunghezza = sizeof(listaInteri) / sizeof(listaInteri[0]); // lunghezza arrey
-    for (int i = 0; i < lunghezza; i++)//scorro arrey1 per pari
+    int lista[5] = {2, 3, 4, 5, 6};                   // creo arrey
+    int lunghezza = sizeof(lista) / sizeof(lista[0]); // lunghezza arrey
+    for (int i = 0; i < lunghezza; i++)               // scorro arrey1 per pari
     {
-        if (listaInteri[i] % 2 == 0)
+        if (lista[i] % 2 == 0)
         {
             quanti++; // lunghezza arrey2
         }
-        else{continue;}
-    }   
-        int listaInteri2[quanti] = {};                                // creo arrey
-        int lun = sizeof(listaInteri2) / sizeof(listaInteri2[0]);  // lunghezza arrey2
-        for (int i = 0; i < lunghezza; i++) // scorro arrey1 perr trovare pari
+        else
         {
-            if (listaInteri[i] % 2 == 0)
-            {
-                listaInteri2[j] = listaInteri[i];
-                j++;
-            }
+            break;
         }
-    
+    }
+    int lista2[quanti] = {j};                     // creo arrey
+    int lun = sizeof(lista2) / sizeof(lista2[0]); // lunghezza arrey2
+    for (int i = 0; i < lunghezza; i++)           // scorro arrey1 perr trovare pari
+    {
+        if (lista[i] % 2 == 0)
+        {
+            lista2[j] = lista[i];
+            j++;
+        }
+        else
+        {
+            break;
+        }
+    }
     for (int i = 0; i < lun; i++) // per lunghezza arrey2 scorro
     {
-        cout << listaInteri2[i] << endl; //stampocol cazzoo
+        cout << lista2[i] << endl; // stampocol cazzoo
     }
+    return 0;
 }
