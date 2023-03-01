f27cde0811df9cb0e795a3e5561c62853ae7d0ad
ma018ma <marzia018@libero.it>
2023-03-01 15:42:09 +0100

    rinominati esercizi

--- esercizio7.cpp
+++ esercizio7.cpp
@@ -1,12 +1,18 @@
+/*Scrivi una funzione che prenda in input un array di interi e 
+la sua lunghezza e restituisca il numero di sequenze di 
+interi crescenti all'interno dell'array. Ad esempio, se 
+l'array di input è {1, 2, 3, 5, 4, 7}, la funzione dovrebbe 
+restituire 2 perché ci sono due sequenze crescenti: {1, 2, 
+3} e {4, 7}.*/
+
 #include <iostream>
 #include <string>
 #include <array>
 using namespace std;
 int main()
 {
-    int listaInteri[spaziArrey] = {numeri};  
-    int lunghezza = sizeof(listaInteri) / sizeof(listaInteri[0]);
-    std::cout << "dammi " << spaziArrey << " numeri" << endl;
+    int lista1[5] = {2,3,6,7,8};  
+    int lun = sizeof(lista1) / sizeof(lista1[0]);
     bool crescente = true;
    
     for (int i = 0; i < lunghezza; i++)
